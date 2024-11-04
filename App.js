435d0726ec84f46150050b722b272f59f3dd40be
Badr Ouaddah <Badr.Ouaddah.07@gmail.com>
2024-11-04 13:52:30 +0100

    Add list in App.js

--- App.js
+++ App.js
@@ -6,7 +6,15 @@ export default function App() {
     const [name, setname] = useState('');
     const handleClick = () => {
     };
-
+    const [people, setpeople] = useState([
+      { name: 'Ahmed', key: '1'},
+      { name: 'Ali', key: '2'},
+      { name: 'Omar', key: '3'},
+      { name: 'Mohammed', key: '4'},
+      { name: 'Saad', key: '5'},
+      { name: 'Rachid', key: '6'},
+      { name: 'Brahim', key: '7'},
+    ])
 
   return (
     <View style={styles.container}>
@@ -19,7 +27,16 @@ export default function App() {
       </View>
       <StatusBar style="auto" />
       <Text></Text>
-      <Text>Hello {name}</Text>
+
+      {people.map((item) => {
+        return(
+          <View key={item.key}>
+            <Text style={styles.item}>
+              {item.name}
+            </Text>
+          </View>
+        )
+      })}
     </View>
   );
 }
@@ -42,5 +59,11 @@ const styles = StyleSheet.create({
     padding: 8,
     margin: 10,
     width: 200,
+  },
+  item: {
+    marginTop: 24,
+    padding: 30,
+    backgroundColor: '#a28089',
+    fontSize: 24
   },
 });

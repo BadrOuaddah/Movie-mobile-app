10ad21043516c213b49e8445e97bc49ab5d45941
Badr Ouaddah <Badr.Ouaddah.07@gmail.com>
2024-11-04 13:56:08 +0100

    Add ScrollView in list

--- App.js
+++ App.js
@@ -1,42 +1,52 @@
 import { StatusBar } from "expo-status-bar";
 import { useState } from "react";
-import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+import {
+  Button,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  ScrollView,
+} from "react-native";
 
 export default function App() {
-    const [name, setname] = useState('');
-    const handleClick = () => {
-    };
-    const [people, setpeople] = useState([
-      { name: 'Ahmed', key: '1'},
-      { name: 'Ali', key: '2'},
-      { name: 'Omar', key: '3'},
-      { name: 'Mohammed', key: '4'},
-      { name: 'Saad', key: '5'},
-      { name: 'Rachid', key: '6'},
-      { name: 'Brahim', key: '7'},
-    ])
+  const [name, setname] = useState("");
+  const handleClick = () => {};
+  const [people, setpeople] = useState([
+    { name: "Ahmed", key: "1" },
+    { name: "Ali", key: "2" },
+    { name: "Omar", key: "3" },
+    { name: "Mohammed", key: "4" },
+    { name: "Saad", key: "5" },
+    { name: "Rachid", key: "6" },
+    { name: "Brahim", key: "7" },
+  ]);
 
   return (
     <View style={styles.container}>
       <Text style={styles.boldText}>Hello World!</Text>
       <Text></Text>
       <Text style={styles.boldText}>Enter your name : </Text>
-      <TextInput style={styles.input} onChangeText={text => setname(text)} placeholder="Name"></TextInput>
+      <TextInput
+        style={styles.input}
+        onChangeText={(text) => setname(text)}
+        placeholder="Name"
+      ></TextInput>
       <View>
         <Button title="Click here" onPress={handleClick} />
       </View>
       <StatusBar style="auto" />
       <Text></Text>
-
-      {people.map((item) => {
-        return(
-          <View key={item.key}>
-            <Text style={styles.item}>
-              {item.name}
-            </Text>
-          </View>
-        )
-      })}
+      <ScrollView>
+        {people.map((item) => {
+          return (
+            <View key={item.key}>
+              <Text style={styles.item}>{item.name}</Text>
+            </View>
+          );
+        })}
+      </ScrollView>
+      <Text></Text>
     </View>
   );
 }
@@ -63,7 +73,7 @@ const styles = StyleSheet.create({
   item: {
     marginTop: 24,
     padding: 30,
-    backgroundColor: '#a28089',
-    fontSize: 24
+    backgroundColor: "#a28089",
+    fontSize: 20,
   },
 });

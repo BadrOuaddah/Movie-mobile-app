c6efd51402eeba051aec48cd6a861f31556500d4
Badr Ouaddah <Badr.Ouaddah.07@gmail.com>
2024-11-04 13:17:41 +0100

    Add handleClick and text input in App.js

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
+    const [name, setname] = useState('');
+    const handleClick = () => {
+    };
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.boldText}>Hello World!</Text>
+      <Text></Text>
+      <Text style={styles.boldText}>Enter your name : </Text>
+      <TextInput style={styles.input} onChangeText={text => setname(text)} placeholder="Name"></TextInput>
+      <View>
+        <Button title="Click here" onPress={handleClick} />
+      </View>
       <StatusBar style="auto" />
+      <Text></Text>
+      <Text>Hello {name}</Text>
     </View>
   );
 }
@@ -13,8 +27,20 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  boldText: {
+    fontWeight: "bold",
+    fontSize: 25,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "#111",
+    padding: 8,
+    margin: 10,
+    width: 200,
   },
 });

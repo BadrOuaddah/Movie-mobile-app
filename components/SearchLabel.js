69d11c54ccdc8e608f60921a7ff860a6f70fd1c0
Badr Ouaddah <Badr.Ouaddah.07@gmail.com>
2024-11-07 17:18:51 +0100

    Update search label

--- components/SearchLabel.js
+++ components/SearchLabel.js
@@ -1,30 +1,39 @@
-import React, { Component } from "react";
-import { TextInput, View, StyleSheet } from "react-native";
+import React from "react";
+import { View, TextInput, StyleSheet } from "react-native";
+import Icon from "react-native-vector-icons/Ionicons";
 
-const SearchLabel = (props) => {
+const SearchBox = ({ value, setSearchValue }) => {
   return (
-    <View style={styles.label}>
-      <TextInput placeholder="Type here to search..." value={props.value} onChange={}/>
+    <View style={styles.inputContainer}>
+      <Icon name="search" size={20} color="#000" style={styles.icon} />
+      <TextInput
+        style={styles.inputField}
+        value={value}
+        onChangeText={setSearchValue}
+        placeholder="Type to search..."
+        placeholderTextColor="#666"
+      />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    padding: 10,
-    backgroundColor: '#f1f1f1',
-    borderRadius: 5,
-    marginVertical: 5,
-  },
-  label: {
-    fontSize: 16,
-    fontWeight: 'bold',
-    borderWidth: 1,
-    borderColor: "#111",
+  inputContainer: {
+    flexDirection: "row",
+    alignItems: "center",
     padding: 8,
-    margin: 10,
-    width: 200,
+    backgroundColor: "#f1f1f1",
+    borderRadius: 5,
+    marginVertical: 10,
+  },
+  icon: {
+    marginRight: 8,
+  },
+  inputField: {
+    flex: 1,
+    fontSize: 16,
+    color: "#333",
   },
 });
 
-export default SearchLabel;
+export default SearchBox;

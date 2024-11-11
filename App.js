db4c8c748b3476183973d8e75179926a36a97c41
Badr Ouaddah <Badr.Ouaddah.07@gmail.com>
2024-11-11 17:14:43 +0100

    Change background color using linear gradient

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { StatusBar } from "expo-status-bar";
 import { useState } from "react";
 import { Button, StyleSheet, Text, View, TextInput, Image } from "react-native";
 import SearchLabel from "./components/SearchLabel";
+import { LinearGradient } from "expo-linear-gradient";
 
 export default function App() {
   const [movie, setMovie] = useState(null);
@@ -28,59 +29,46 @@ export default function App() {
   };
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.boldText}>E-Movie application</Text>
-      {/* <SearchLabel></SearchLabel> */}
-      <Text></Text>
-      <SearchLabel value={movieTitle} setSearchValue={setMovieTitle} />
-      <Text></Text>
-      <View>
-        <Button title="Click here" onPress={handleClick} />
-      </View>
-      <Text></Text>
-
-      <Text></Text>
-      <StatusBar style="auto" />
-      {movie && (
-        <View style={styles.movieDetails}>
-          <Image source={{ uri: movie.Poster }} style={styles.image} />
-          <Text style={styles.title}>{movie.Title}</Text>
-          <Text>Year: {movie.Year}</Text>
-          <Text>Genre: {movie.Genre}</Text>
-          <Text>Language: {movie.Language}</Text>
-          <Text>Country: {movie.Country}</Text>
+    <LinearGradient colors={["#2c3e50", "#bdc3c7"]} style={styles.gradient}>
+      <View style={styles.container}>
+        <Text style={styles.boldText}>E-Movie Application</Text>
+        <SearchLabel value={movieTitle} setSearchValue={setMovieTitle} />
+        <View>
+          <Button title="Click here" onPress={handleClick} />
         </View>
-      )}
-      <Text></Text>
-    </View>
+        <Text></Text>
+        <Text></Text>
+        <StatusBar style="auto" />
+        {movie && (
+          <View style={styles.movieDetails}>
+            <Image source={{ uri: movie.Poster }} style={styles.image} />
+            <Text style={styles.title}>{movie.Title}</Text>
+            <Text>Year: {movie.Year}</Text>
+            <Text>Genre: {movie.Genre}</Text>
+            <Text>Language: {movie.Language}</Text>
+            <Text>Country: {movie.Country}</Text>
+          </View>
+        )}
+      </View>
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
+  gradient: {
+    flex: 1,
+    justifyContent: "center"
+  },
   container: {
     flex: 1,
-    backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
     paddingHorizontal: 30
   },
-
   boldText: {
     fontWeight: "bold",
-    fontSize: 25
-  },
-  input: {
-    borderWidth: 1,
-    borderColor: "#111",
-    padding: 8,
-    margin: 10,
-    width: 200
-  },
-  item: {
-    marginTop: 24,
-    padding: 30,
-    backgroundColor: "#a28089",
-    fontSize: 20
+    fontSize: 25,
+    color: "#fff"
   },
   image: {
     width: 200,

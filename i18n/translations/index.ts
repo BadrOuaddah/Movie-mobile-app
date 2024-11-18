04dd4ee5a37baeed491d9447d5ac3df337c8c06a
Badr Ouaddah <Badr.Ouaddah.07@gmail.com>
2024-11-18 11:36:12 +0100

    Create i18n configuration

--- i18n/translations/index.ts
+++ i18n/translations/index.ts
@@ -1,4 +1,4 @@
-export {default as en} from './en.json'
-export {default as fr} from './fr.json'
-export {default as es} from './es.json'
-export {default as it} from './it.json'
+export { default as en } from "./en.json";
+export { default as fr } from "./fr.json";
+export { default as es } from "./es.json";
+export { default as it } from "./it.json";

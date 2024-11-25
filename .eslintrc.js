b78cfdf9d245b09450776f2977010d5f549a00d8
Badr Ouaddah <Badr.Ouaddah.07@gmail.com>
2024-11-25 16:11:06 +0100

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,47 +1,34 @@
 module.exports = {
-	"env": {
-		"browser": true,
-		"es2021": true
-	},
-	"extends": "eslint:recommended",
-	"overrides": [
-		{
-			"env": {
-				"node": true
-			},
-			"files": [
-				".eslintrc.{js,cjs}"
-			],
-			"parserOptions": {
-				"sourceType": "script"
-			}
-		}
-	],
-	"parser": "@babel/eslint-parser",
-	"parserOptions": {
-		"ecmaVersion": "latest",
-		"sourceType": "module",
-		"requireConfigFile": false,
-		"ecmaFeatures": {
-			"jsx": true
-		}
-	},
-	"rules": {
-		"indent": [
-			"error",
-			"tab"
-		],
-		"linebreak-style": [
-			"error",
-			"windows"
-		],
-		"quotes": [
-			"error",
-			"double"
-		],
-		"semi": [
-			"error",
-			"always"
-		]
-	}
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: ["eslint:recommended", "plugin:react/recommended"],
+  overrides: [
+    {
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script"
+      }
+    }
+  ],
+  parser: "@babel/eslint-parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    requireConfigFile: false,
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  plugins: ["react"],
+  rules: {
+    indent: ["error", "tab", { SwitchCase: 1 }],
+    "linebreak-style": ["error", "windows", "unix"],
+    quotes: ["error", "double"],
+    semi: ["error", "always"],
+    "react/react-in-jsx-scope": "off",
+    "no-unused-vars": ["warn", { argsIgnorePattern: "^_" }]
+  }
 };
